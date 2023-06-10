cfe41337015c7c7159a446619ff6123fac478bcf
akshayachuthan <akshayachuthan@credenceanalytics.com>
2023-06-10 07:05:46 +0530

    fixes for timeline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,7 +34,11 @@
                     class="w-24 h-24 bg-gray-100 mx-auto object-cover object-center flex-shrink-0 rounded-lg"
                     src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
                   /> -->
-                  <div class="p-4 mx-auto bg-gray-200 ring-1 ring-gray-500 ring-offset-2 rounded-full">AB</div>
+                  <div
+                    class="p-4 mx-auto bg-gray-200 ring-1 ring-gray-500 ring-offset-2 rounded-full"
+                  >
+                    AB
+                  </div>
                 </div>
               </div>
               <div class="text-xs font-bold text-center w-full mt-4">
@@ -121,7 +125,7 @@
           <div class="user-posts flex flex-col overflow-y-auto h-96">
             <div
               class="post-card border border-gray-300 shadow-md rounded-md px-4 py-3 mt-3"
-              v-for="(item, index) in [1, 2, 3, 4, 5, 6, 7]"
+              v-for="(item, index) in userTimeline"
               :key="index"
             >
               <div class="w-full py-2">
@@ -133,7 +137,7 @@
                   />
                   <div class="flex-grow">
                     <h2 class="text-gray-900 title-font text-xs font-bold">
-                      Holden Caulfield
+                      {{ item.userName }}
                     </h2>
                     <p class="text-gray-500 text-xs">UI Designer</p>
                   </div>
@@ -196,6 +200,38 @@
                   </svg>
                 </div>
               </div>
+              <div class="flex items-center mt-4">
+                <img
+                  class="object-cover w-4 h-4 -mx-1 border-2 border-white rounded-full dark:border-gray-700 shrink-0"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80"
+                  alt=""
+                />
+                <img
+                  class="object-cover w-4 h-4 -mx-1 border-2 border-white rounded-full dark:border-gray-700 shrink-0"
+                  src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80"
+                  alt=""
+                />
+                <img
+                  class="object-cover w-4 h-4 -mx-1 border-2 border-white rounded-full dark:border-gray-700 shrink-0"
+                  src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1256&q=80"
+                  alt=""
+                />
+                <img
+                  class="object-cover w-4 h-4 -mx-1 border-2 border-white rounded-full dark:border-gray-700 shrink-0"
+                  src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80"
+                  alt=""
+                />
+                <p
+                  class="flex items-center justify-center w-4 h-4 -mx-1 text-xs text-blue-600 bg-blue-100 border-2 border-white rounded-full"
+                >
+                  +4
+                </p>
+                <div class="text-xs">
+                  &nbsp; Liked by
+                  <b class="cursor-pointer" @click="showModal">Komal Patil</b>
+                  and 1,143
+                </div>
+              </div>
               <div class="text-xs font-medium text-left text-gray-600 mt-4">
                 Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
                 gentrify, subway tile poke farm-to-table. Franzen you probably
@@ -251,6 +287,62 @@
         </div>
       </div>
     </section>
+    <div
+      v-if="isModalVisible"
+      class="fixed inset-0 flex items-center justify-center z-50"
+    >
+      <div class="bg-white rounded shadow-lg p-4 w-72">
+        <!-- Modal content goes here -->
+        <h2 class="text-sm font-bold mb-4">Likes and Dislikes</h2>
+        <div class="flex flex-col">
+          <div class="flex flex-row justify-between items-center">
+            <div class="text-sm font-bold">Komal patil</div>
+            <div class="flex flex-row items-center">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+                  />
+                </svg>
+              </div>
+              <div class="ml-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row justify-end items-center">
+          <button
+            @click="hideModal"
+            class="bg-white border border-gray-300 text-xs text-black px-4 py-2 rounded mt-4"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -261,6 +353,20 @@ export default {
   name: "HomeView",
   components: {
     "nav-bar": Navbar,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+      userTimeline: [],
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    hideModal() {
+      this.isModalVisible = false;
+    },
   },
 };
 </script>
