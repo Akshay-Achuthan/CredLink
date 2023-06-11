f788837ae3d31fcd40d254361c885f987b322496
akshayachuthan <akshayachuthan@credenceanalytics.com>
2023-06-11 21:32:13 +0530

    bug fixes validation fix and css changes

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,58 +1,33 @@
 <template>
   <div class="app-signin">
     <!-- component -->
-    <div class="h-screen flex">
+     <div class="h-screen flex">
       <div
-        class="flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center"
+        class="flex w-1/2 bg-gradient-to-tr from-red-800 to-orange-300 i justify-around items-center"
       >
-        <div>
-          <h1 class="text-white font-bold text-4xl font-sans">CREDLink</h1>
-          <p class="text-white mt-1">
-            The most popular peer to peer lending at SEA
+        <div v-if="!showReadMore">
+          <h1 class="logo-style text-white font-bold text-4xl font-sans">CREDLink</h1>
+          <p class="text-white mt-2">
+            Connect. Share. Grow.
           </p>
           <button
             type="submit"
-            class="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+            class="block w-28 bg-white text-red-500 mt-4 py-2 rounded-2xl font-bold mb-2"
+            @click="handleClickReadMore"
           >
             Read More
           </button>
+        </div>
+        <div v-if="showReadMore" :class="showReadMore ? 'read-more-container.show' : 'read-more-container'">
+          <p class="text-white text-1xl " style="width:85%; margin:auto;">
+            CREDLink is a social media platform designed to connect professionals and facilitate knowledge sharing and growth. With CREDLink, you can expand your professional network, share valuable insights and experiences, and collaborate with like-minded individuals in your industry.
+          </p>
         </div>
       </div>
       <div class="flex w-1/2 justify-center items-center bg-white">
         <form class="bg-white">
           <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
           <p class="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
-          <!-- <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-            <input
-              class="pl-2 outline-none border-none"
-              type="email"
-              name=""
-              id=""
-              v-model="emailEntered"
-              placeholder="Email Address"
-            />
-          </div> -->
-          <!-- <button
-            type="submit"
-            @click="validateMail"
-            class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-4"
-          >
-            Login
-          </button> -->
           <button
             @click="handleClickSignIn"
             class="px-4 py-2 cursor-pointer border flex gap-2 border-gray-300 shadow-lg rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
@@ -75,12 +50,16 @@ export default {
     return {
       isSignIn: false,
       showAlert:false,
-      alertMessage:'please enter a valid email id',
-      emailEntered:'',
+      alertMessage:'Please login using credence email id',
+      showReadMore: false,
     };
   },
   
   methods: {
+    handleClickReadMore() {
+      this.showReadMore = true;
+    },
+
     async handleClickSignIn() {
       try {
         const googleUser = await this.$gAuth.signIn();
@@ -92,47 +71,42 @@ export default {
           'lastName': googleUser.getBasicProfile().nY,
           'email': googleUser.getBasicProfile().gw
         }
-        
-    fetch("http://192.168.0.210:2100/user/signup",
-    {
-     method: "POST",
-     headers:{'content-type': 'application/json'},
-     body: JSON.stringify({
-      "f_name": userProfile.firstName,
-      "l_name": userProfile.lastName,
-      "email": userProfile.email
-     }),
- })
-    .then(response => response.json())
-    .then(async response => {
-      console.log('response',response);
-      if(response.status == "success"){
-        sessionStorage.setItem('user_id', response.user_id);
-        this.$router.push('/');
-        location.reload();
-      }
-    })
-    .catch(error => {
-      console.error("There was an error!", error);
-    });
 
-
-        this.isSignIn = this.$gAuth.isAuthorized;
-        console.log('userProfile',userProfile);
-      } catch (error) {
-        console.error(error);
-        return null;
-      }
-    },
-
-    validateMail(){
-      if(this.emailEntered.includes('credenceanalytics')){
-        console.log("success"); // change is needed
+      if(userProfile.email.includes('credenceanalytics')){
+        fetch("http://192.168.0.210:2100/user/signup",
+          {
+          method: "POST",
+          headers:{'content-type': 'application/json'},
+          body: JSON.stringify({
+            "f_name": userProfile.firstName,
+            "l_name": userProfile.lastName,
+            "email": userProfile.email
+          }),
+      })
+          .then(response => response.json())
+          .then(async response => {
+            // console.log('response',response);
+            if(response.status == "success"){
+              sessionStorage.setItem('user_id', response.user_id);
+              this.$router.push('/');
+              location.reload();
+            }
+          })
+          .catch(error => {
+            console.error("There was an error!", error);
+          });
       }else{
         this.showAlert =  true;
         setTimeout(() => {
           this.showAlert = false;
         }, 3000);
+      }
+  
+        this.isSignIn = this.$gAuth.isAuthorized;
+        // console.log('userProfile',userProfile);
+      } catch (error) {
+        console.error(error);
+        return null;
       }
     },
   },
@@ -163,4 +137,19 @@ export default {
   }
 }
 
+.read-more-container {
+  opacity: 0;
+  max-height: 0;
+  overflow: hidden;
+  transition: opacity 0.3s ease-out, max-height 0.3s ease-out;
+}
+
+.read-more-container.show {
+  opacity: 1;
+  max-height: 1000px;
+}
+
+.logo-style{
+  letter-spacing: 5px;
+}
 </style>
